30021f541a7ce56539cd87b09dccfbb667cc5b70
lehoanganh75 <gaokun2021@gmail.com>
2025-01-08 23:30:30 +0700

    Finish JSF Part1 Question1

---
+++ Question1/Question1.js
@@ -0,0 +1,36 @@
+var weightMark = 78
+var heightMark = 1.69
+
+var BMIMark = weightMark / (heightMark*heightMark)
+
+console.log("Can nang cua Mark: " + weightMark);
+console.log("Chieu cao cua Mark: " + heightMark);
+console.log("BMI cua Mark: " + BMIMark);
+
+var weightJohn = 92
+var heightJohn = 1.95
+
+var BMIJohn = weightJohn / (heightJohn*heightJohn)
+
+console.log("Can nang cua John: " + weightJohn);
+console.log("Chieu cao cua John: " + heightJohn);
+console.log("BMI cua John: " + BMIJohn);
+
+var weightMark = 95
+var heightMark = 1.88
+
+var BMIMark = weightMark / (heightMark*heightMark)
+
+console.log("Can nang cua Mark: " + weightMark);
+console.log("Chieu cao cua Mark: " + heightMark);
+console.log("BMI cua Mark: " + BMIMark);
+
+var weightJohn = 85
+var heightJohn = 1.76
+
+var BMIJohn = weightJohn / (heightJohn*heightJohn)
+
+console.log("Can nang cua John: " + weightJohn);
+console.log("Chieu cao cua John: " + heightJohn);
+console.log("BMI cua John: " + BMIJohn);
+
